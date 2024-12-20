0dcab9db9df28b12e5662dbe38976b508866908a
sbalak <bsidharth@live.com>
2024-12-20 22:24:27 +0530

    unnecessary usings removed

--- Shopper.Web/Program.cs
+++ Shopper.Web/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
